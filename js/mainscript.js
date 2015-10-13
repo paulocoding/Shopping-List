3b38aa3e3ba625247e480b8f0412e1ec179e1eee
Paulo Silva <engelik@gmail.com>
2015-10-13 13:38:11 +0100

    event handling and clean up

--- js/mainscript.js
+++ js/mainscript.js
@@ -1,8 +1,20 @@
-function addToList () {
-	var shopList = document.getElementsByTagName('ul')[0];
-	var shopItem = document.getElementById('shopItem').value;	
-	document.getElementById('shopItem').value = '';
-	shopList.innerHTML += '<li>'+shopItem+'</li>';
-}
-
-document.getElementById('addToList').onclick = addToList;
+$(document).ready(function(){
+	
+	$('#addToList').click(function () {
+		var shopItem = document.getElementById('shopItem').value;
+		if (shopItem){
+			document.getElementById('shopItem').value = '';
+			$('.list ul').append('<li>'+shopItem+'<span class="deleteBtn">X</span></li>');
+		}
+	});
+	
+	document.getElementsByTagName('ul')[0].addEventListener('click', removeItem, false);
+	
+	function removeItem(e) {
+		var target = e.target.parentNode;
+		if(target.nodeName === 'LI') {
+				$(target).remove();
+		}
+	};
+	
+});
